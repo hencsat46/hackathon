661429e673ef2528f8a6ac6bc7a4e9c90ac8d35b
VanLavr <lavr.i-n@mail.ru>
2024-05-21 19:27:27 +0300

    feat: added CreateToken method

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -2,7 +2,10 @@ package jwt
 
 import (
 	"hackathon/pkg/config"
+	"log/slog"
 	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type JWT struct {
@@ -15,4 +18,19 @@ func New(cfg *config.Config) *JWT {
 		secret:  cfg.JWTsecret,
 		expTime: time.Duration(cfg.ExpTime),
 	}
+}
+
+func (j *JWT) CreateToken(guid string) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"guid":      guid,
+		"exp":       time.Now().Add(j.expTime).Unix(),
+		"createdAt": time.Now().Unix(),
+	})
+
+	stringToken, err := token.SignedString([]byte(j.secret))
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	return stringToken
 }
