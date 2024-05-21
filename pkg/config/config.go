423b4cd863058236ee4dbb52a3f21c420e2890d2
VanLavr <lavr.i-n@mail.ru>
2024-05-21 18:27:32 +0300

    feat: added config constructor

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,22 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	Environment string
+}
+
+func New() *Config {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal(err)
+	}
+
+	return &Config{
+		Environment: os.Getenv("ENV"),
+	}
 }
