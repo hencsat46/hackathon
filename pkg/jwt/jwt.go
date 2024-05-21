443468fc8694ac4e9d842379e8136b67e170613e
VanLavr <lavr.i-n@mail.ru>
2024-05-21 19:22:48 +0300

    feat: added jwt and constructor

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,1 +1,18 @@
 package jwt
+
+import (
+	"hackathon/pkg/config"
+	"time"
+)
+
+type JWT struct {
+	secret  string
+	expTime time.Duration
+}
+
+func New(cfg *config.Config) *JWT {
+	return &JWT{
+		secret:  cfg.JWTsecret,
+		expTime: cfg.ExpTime,
+	}
+}
