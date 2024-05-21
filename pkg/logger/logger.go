36149bd5a82ffaf44aad890097192fcc6527d169
VanLavr <lavr.i-n@mail.ru>
2024-05-21 18:22:18 +0300

    feat: added logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,22 @@
 package logger
+
+import (
+	"log"
+	"log/slog"
+
+	"github.com/jcmturner/gokrb5/v8/config"
+)
+
+type Logger struct {
+	l *slog.Logger
+}
+
+func New(cfg *config.Config) *Logger {
+	if cfg.Environment == "dev" {
+
+	} else if cfg.Environment == "prod" {
+
+	} else {
+		log.Fatal("choose the environment")
+	}
+}
