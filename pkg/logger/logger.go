7fe95cab6b96c47349339530e859c923ecc02f8f
VanLavr <lavr.i-n@mail.ru>
2024-05-21 18:26:06 +0300

    feat: added constructor for logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,8 +3,9 @@ package logger
 import (
 	"log"
 	"log/slog"
+	"os"
 
-	"github.com/jcmturner/gokrb5/v8/config"
+	"hackathon/pkg/config"
 )
 
 type Logger struct {
@@ -13,10 +14,23 @@ type Logger struct {
 
 func New(cfg *config.Config) *Logger {
 	if cfg.Environment == "dev" {
-
+		log := new(Logger)
+		log.l = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     slog.LevelDebug,
+		}))
+		return log
 	} else if cfg.Environment == "prod" {
+		log := new(Logger)
+		log.l = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: false,
+			Level:     slog.LevelInfo,
+		}))
 
+		return log
 	} else {
 		log.Fatal("choose the environment")
 	}
+
+	return nil
 }
