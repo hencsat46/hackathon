bea4ebf9fd7fe732aa9b8e544c9276ee7cf6a6f6
VanLavr <lavr.i-n@mail.ru>
2024-05-22 19:07:46 +0300

    feat&fix&refactor: refactored business layer

---
+++ internal/business/messageService/messageService.go
@@ -0,0 +1,58 @@
+package messageservice
+
+import (
+	"context"
+	"hackathon/models"
+	"log/slog"
+)
+
+type MessageService struct {
+	MessageDataAccess IDataAccessMessage
+}
+
+type IDataAccessMessage interface {
+	FetchMessagesForChatroom(ctx context.Context, chatroomID string) ([]models.Message, error)
+	CreateMessage(ctx context.Context, message models.Message) error
+	UpdateMessage(ctx context.Context, newContent, messageID string) error
+	DeleteMessage(ctx context.Context, messageID string) error
+}
+
+func New(msgDao IDataAccessMessage) *MessageService {
+	return &MessageService{
+		MessageDataAccess: msgDao,
+	}
+}
+
+func (b *MessageService) FetchMessagesForChatroom(ctx context.Context, chatroomID string) ([]models.Message, error) {
+	messages, err := b.MessageDataAccess.FetchMessagesForChatroom(ctx, chatroomID)
+	if err != nil {
+		slog.Debug(err.Error())
+		return nil, err
+	}
+
+	return messages, nil
+}
+
+func (b *MessageService) CreateMessage(ctx context.Context, message models.Message) error {
+	if err := b.MessageDataAccess.CreateMessage(ctx, message); err != nil {
+		slog.Debug(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (b *MessageService) UpdateMessage(ctx context.Context, newContent, messageID string) error {
+	if err := b.MessageDataAccess.UpdateMessage(ctx, newContent, messageID); err != nil {
+		slog.Debug(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (b *MessageService) DeleteMessage(ctx context.Context, messageID string) error {
+	if err := b.MessageDataAccess.DeleteMessage(ctx, messageID); err != nil {
+		slog.Debug(err.Error())
+		return err
+	}
+	return nil
+}
---
+++ internal/business/userService/serviceUser.go
@@ -0,0 +1,96 @@
+package userservice
+
+import (
+	"context"
+	"fmt"
+	"hackathon/models"
+	"log/slog"
+
+	"github.com/beevik/guid"
+)
+
+type UserService struct {
+	UserDao IDataAccessUser
+}
+
+type IDataAccessUser interface {
+	FetchUserChatrooms(ctx context.Context, GUID string) ([]models.Chatroom, error)
+	Login(ctx context.Context, user models.User) (string, error)
+	CreateUser(ctx context.Context, user models.User) error
+	UpdateUsername(ctx context.Context, username, GUID string) error
+	UpdateEmail(ctx context.Context, email, GUID string) error
+	UpdatePassword(ctx context.Context, oldPassword, newPassword, GUID string) error
+	DeleteUser(ctx context.Context, GUID string) error
+}
+
+func New(userdao IDataAccessUser) *UserService {
+	return &UserService{
+		UserDao: userdao,
+	}
+}
+
+func (b *UserService) FetchUserChatrooms(ctx context.Context, GUID string) ([]models.Chatroom, error) {
+	slog.Debug(fmt.Sprintf("fetching chatrooms: %v", GUID))
+
+	chatrooms, err := b.UserDao.FetchUserChatrooms(ctx, GUID)
+	if err != nil {
+		slog.Debug(err.Error())
+		return nil, err
+	}
+
+	return chatrooms, nil
+}
+
+func (b *UserService) Login(ctx context.Context, user models.User) (string, error) {
+	guid, err := b.UserDao.Login(ctx, user)
+	if err != nil {
+		slog.Debug(err.Error())
+		return "", err
+	}
+
+	return guid, nil
+}
+
+func (b *UserService) CreateUser(ctx context.Context, user models.User) (string, error) {
+	user.GUID = guid.NewString()
+
+	err := b.UserDao.CreateUser(ctx, user)
+	if err != nil {
+		slog.Debug(err.Error())
+		return "", err
+	}
+
+	return user.GUID, nil
+}
+
+func (b *UserService) UpdateUsername(ctx context.Context, username, GUID string) error {
+	if err := b.UserDao.UpdateUsername(ctx, username, GUID); err != nil {
+		slog.Debug(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (b *UserService) UpdateEmail(ctx context.Context, email, GUID string) error {
+	if err := b.UserDao.UpdateEmail(ctx, email, GUID); err != nil {
+		slog.Debug(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (b *UserService) UpdatePassword(ctx context.Context, oldPassword, newPassword, GUID string) error {
+	if err := b.UserDao.UpdatePassword(ctx, oldPassword, newPassword, GUID); err != nil {
+		slog.Debug(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (b *UserService) DeleteUser(ctx context.Context, GUID string) error {
+	if err := b.UserDao.DeleteUser(ctx, GUID); err != nil {
+		slog.Debug(err.Error())
+		return err
+	}
+	return nil
+}
